0b4c1c5fe52dca084238bf34084cfe398a65be43
Andjelka Andric <andricandjelka@gmail.com>
2024-02-06 15:20:34 +0100

    dodati importi

--- app/Http/Resources/EventResource.php
+++ app/Http/Resources/EventResource.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Resources;
 use Illuminate\Http\Request;
+use App\Models\Event;
+use App\Models\TypeEvent;
+use App\Models\User;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class EventResource extends JsonResource
@@ -13,22 +16,16 @@ class EventResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-       // return parent::toArray($request);
-       return [
-
-    
-    
-        'id' => $this->resource->id,
-       'name' => $this->resource->name,
-       //
-       //'datePaid'=> $this->resource->datePaid,
-       
-       'amount'=>$this->resource->amount,
-       'user'=> new UserResource($this->resource->user),
-       'type'=>new TypeEventResource($this->resource->type),
-       'eventDate' => $this->resource->eventDate,
-
-
-       ];
+        $user = User::find($this->user_id);
+        $type=TypeEvent::find($this->type_id);
+        return [
+         'id' => $this->resource->id,
+        'name' => $this->resource->name,
+        
+        'amount'=>$this->resource->amount,
+        'user'=> new UserResource($user),
+        'type'=>new TypeEventResource($type),
+        'eventDate' => $this->resource->eventDate,
+        ];
     }
 }
--- database/seeders/EventRandomDataSeeder.php
+++ database/seeders/EventRandomDataSeeder.php
@@ -18,7 +18,7 @@ class EventRandomDataSeeder extends Seeder
         for ($i = 0; $i < 100; $i++) {
             Event::create([
                 'name' => $faker->name(),
-                'eventDate' => now(),
+                'eventDate' => $faker->unique()->dateTimeInInterval($startDate = '-7 days', $interval = '+ 5 days', $timezone = null),
                 'amount' => $faker->numberBetween($min = 100, $max = 100000),
                 'user_id' => rand(1,3),
                 'type_id' => rand(1,5)
